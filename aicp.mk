16f92b7d740dfbdbc3da155b2864f21d68216750
Verevka <verevka86@gmail.com>
2017-10-25 12:29:05 +0300

    init Aicp

--- aicp.mk
+++ aicp.mk
@@ -19,18 +19,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from Xiaomi MI6 device
 $(call inherit-product, device/xiaomi/sagit/device.mk)
 
-# Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 
-PRODUCT_NAME := aosp_sagit
+PRODUCT_NAME := aicp_sagit
 PRODUCT_DEVICE := sagit
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 6
 PRODUCT_MANUFACTURER := Xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="Xiaomi/sagit/sagit:7.1.1/NMF26X/7.9.7:user/release-keys"
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
