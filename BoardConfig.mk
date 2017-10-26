ebed741c3d36feff667b4fa63366465801f43f24
Verevka <verevka86@gmail.com>
2017-10-26 19:28:47 +0300

    sagit: add lineagehw

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,11 +111,9 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
-# CM Hardware
+# Lineage Hardware
 BOARD_HARDWARE_CLASS += \
-    hardware/cyanogen/cmhw \
-    $(DEVICE_PATH)/cmhw
-BOARD_USES_CYANOGEN_HARDWARE := true
+    $(DEVICE_PATH)/lineagehw
 TARGET_TAP_TO_WAKE_NODE :=  "/proc/touchpanel/double_tap_enable"
 
 # CNE and DPM
