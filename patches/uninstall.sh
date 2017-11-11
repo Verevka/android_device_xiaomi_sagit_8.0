98f88f928303642acb6cae8592cb04cc7f08b186
Verevka <verevka86@gmail.com>
2017-11-11 10:57:33 +0300

    add LiveDisplay

--- patches/uninstall.sh
+++ patches/uninstall.sh
@@ -2,7 +2,7 @@ echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="frameworks/base hardware/interfaces hardware/libhardware hardware/qcom/audio-caf/msm8998"
+dirs="frameworks/base hardware/interfaces hardware/libhardware hardware/lineage/livedisplay hardware/qcom/audio-caf/msm8998 lineage-sdk"
 
 for dir in $dirs ; do
 	cd $rootdirectory
