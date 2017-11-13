219cf438cf3bf938db96bb8dd0ac07a985335059
Verevka <verevka86@gmail.com>
2017-11-14 00:05:03 +0300

    init: depend on libselinux_defaults

--- init/Android.mk
+++ init/Android.mk
@@ -19,11 +19,12 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := system/core/init
+LOCAL_C_INCLUDES := system/core/init external/selinux/libselinux/include
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_SRC_FILES := init_sagit.cpp
 LOCAL_MODULE := libinit_sagit
 LOCAL_STATIC_LIBRARIES := \
-    libbase
+     libbase \
+     libselinux
 
 include $(BUILD_STATIC_LIBRARY)
